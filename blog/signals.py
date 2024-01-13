05c4949bb35e7ac3d9db11f3558f4603b4eb6b24
Ali <aliahmadi.xx90@gmail.comOmit>
2024-01-13 18:17:09 +0330

    add signal for send mail when create comment

--- blog/signals.py
+++ blog/signals.py
@@ -1,6 +1,10 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Like,Blog
+from .models import Like,Comment
+from django.conf import settings
+
+from django.core.mail import send_mail
+
 
 @receiver(post_save,sender=Like)
 def addlike(created,instance,**kwarg):
@@ -16,3 +20,15 @@ def addlike(created,instance,**kwarg):
         blog.save()
         
     
+@receiver(post_save,sender=Comment)
+def sendemail(created,instance,**kwarg):
+    if created:
+        blog = instance.blog
+        subject = 'Fatemeh site'
+        message = f'comment for this Blog: "{blog.titel}" '
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [settings.EMAIL_HOST_USER]
+        send_mail( subject, message, email_from, recipient_list
+                  ,fail_silently=False, auth_user=None
+                  , auth_password=None, connection=None
+                  , html_message=None)
