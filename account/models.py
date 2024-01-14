8bfab5b9435a777d42560defd1f70edf878cffec
Ali <aliahmadi.xx90@gmail.comOmit>
2024-01-14 15:55:41 +0330

    add "help_text" for email

--- account/models.py
+++ account/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from blog.models import Blog
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), help_text='برای اطلاع از ایجاد وبلاگ جدید')
