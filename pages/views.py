e9f25e8b6351e2cb68df8ce0d6c262e0029af698
Ali <aliahmadi.xx90@gmail.comOmit>
2024-01-21 21:21:44 +0330

    add view for "allblog"-"detaildeblog" and "homepage"

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from blog.models import Blog
 # Create your views here.
 
 def homepage(request):
-    
-    return render(request,'pages/homepage.html')
+    a = Blog.objects.all()[:5]
+    return render(request,'base.html',{'blogs':a})
+
+def detaleblog(request,pk):
+    a = Blog.objects.get(id=pk)
+
+    return render(request,'pages/blogdetaile.html',{'blogdetale':a})
+
+def allblog(request):
+    a = Blog.objects.all()
+    context = {'bloglist':a}
+    print(a, context)
+    return render(request, 'pages/bloglist.html', context)
